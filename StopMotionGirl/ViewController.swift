31ba4173365e5c2aecd39302df7ab92613569d10
Chang Sophia <you@example.com>
2019-04-17 11:03:47 +0800

    Initial Commit

---
+++ StopMotionGirl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopMotionGirl
+//
+//  Created by Chang Sophia on 4/17/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
